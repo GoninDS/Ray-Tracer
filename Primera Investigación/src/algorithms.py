b633898c88ff6113ed17b0a6de9ba87cb81df40b
Luis <luisdi2509@gmail.com>
2023-08-22 21:12:48 -0600

    Algoritmo de linea y circulo

---
+++ Primera Investigación/src/algorithms.py
@@ -0,0 +1,81 @@
+import turtle
+
+# ----------------- Algoritmo de linea -----------------
+# Generado por medio de ChatGPT y editado posteriormente
+def bresenham_line():
+  x0 = int(input("Inserte la coordenada x para el primer punto: "))
+  y0 = int(input("Inserte la coordenada y para el primer punto: "))
+  x1 = int(input("Inserte la coordenada x para el segundo punto: "))
+  y1 = int(input("Inserte la coordenada y para el primer punto: "))
+
+  dx = x1 - x0
+  dy = y1 - y0
+  x = x0
+  y = y0
+  p = 2 * dy - dx
+  
+  while x < x1:
+      turtle.penup()
+      turtle.goto(x, y)
+      turtle.pendown()
+      turtle.dot(5, "red")
+      
+      if p >= 0:
+          y += 1
+          p += 2 * dy - 2 * dx
+      else:
+          p += 2 * dy
+      x += 1
+  turtle.done()
+
+# ----------------- Algoritmo de circulo -----------------
+def draw_circle(xc, yc, x, y):
+	turtle.penup()
+	turtle.goto(xc + x, yc + y)
+	turtle.pendown()
+	turtle.dot(5, "red")
+	turtle.penup()
+	turtle.goto(xc - x, yc + y)
+	turtle.pendown()
+	turtle.dot(5, "red")
+	turtle.penup()
+	turtle.goto(xc + x, yc - y)
+	turtle.pendown()
+	turtle.dot(5, "red")
+	turtle.penup()
+	turtle.goto(xc - x, yc - y)
+	turtle.pendown()
+	turtle.dot(5, "red")
+	turtle.penup()
+	turtle.goto(xc + y, yc + x)
+	turtle.pendown()
+	turtle.dot(5, "red")
+	turtle.penup()
+	turtle.goto(xc - y, yc + x)
+	turtle.pendown()
+	turtle.dot(5, "red")
+	turtle.penup()
+	turtle.goto(xc + y, yc - x)
+	turtle.pendown()
+	turtle.dot(5, "red")
+	turtle.penup()
+	turtle.goto(xc - y, yc - x)
+	turtle.pendown()
+	turtle.dot(5, "red")
+
+def bresenham_circle():
+	r = int(input("Inserte el radio: "))
+	xc = 0
+	yc = 0
+	x = 0
+	y = r
+	d = 3 - 2 * r
+	draw_circle(xc, yc, x, y)
+	while y >= x:
+		x += 1
+		if d > 0:
+			y -= 1
+			d = d + 4 * (x - y) + 10
+		else:
+			d = d + 4 * x + 6
+		draw_circle(xc, yc, x, y)
--- Primera Investigación/src/main.py
+++ Primera Investigación/src/main.py
@@ -1,3 +1,4 @@
+import algorithms as alg
 
 def menu_instructions():
   print("\n-------- Menu --------")
@@ -11,6 +12,22 @@ def menu_instructions():
   print("6. Octagono")
   print("7. Estrella")
 
+def handle_cases(value):
+  if value == 1:
+    alg.bresenham_line()
+  elif value == 2:
+    alg.bresenham_circle()
+  elif value == 3:
+      pass
+  elif value == 4:
+    pass
+  elif value == 5:
+    pass
+  elif value == 6:
+    pass
+  elif value == 7:
+    pass
+
 # Set for the loop
 input_value = 1
 
@@ -18,7 +35,7 @@ while input_value > 0 and input_value < 8:
   menu_instructions()
   try:
     input_value = int(input())
-    
+    handle_cases(input_value)
   except:
     print("\nPor favor inserte un valor valido")
     input_value = 1
