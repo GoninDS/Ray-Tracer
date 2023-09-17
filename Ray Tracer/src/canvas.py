fd9f2df902222d36933a72ba7a0e44043ac0153a
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-09-17 04:46:35 -0600

    Initial incomplete implementation of Canvas class

--- Ray Tracer/src/canvas.py
+++ Ray Tracer/src/canvas.py
@@ -1,1 +1,50 @@
 # Copyright 2023 Luis David Solano Santamaría, Kenneth Daniel Villalobos Solís
+
+# TODO(Kenneth): Help i dont remember how to include in python
+import Color
+
+class Canvas:
+  # Default constructor
+  def __init__(self, height, width):
+    self.height = height
+    self.width = width
+    self.pixels = \
+      [[1 for column in range(width)] for row in range(height)]
+  
+    
+  # Debugging representation
+  def __repr__(self):
+    # Returns how to construct a canvas of the same dimensions
+    return 'Canvas({}, {})'.format(self.height, self.width)
+  
+  # String representation
+  # TODO(Kenneth): Ask Luis if we should print like this or in ppm format
+  def __str__(self): 
+     # Create an string with the dimensions
+    canvas_str = str(self.heigh) + " " +  str(self.width) + "\n"
+    # Move through all the elements in the canvas
+    for row in range(self.height):
+      for column in range(self.width):
+        # Add the element to the str
+        canvas_str += str(self.pixels[row][column])
+        # If is not the last element of the row
+        if column != self.width-1:
+          canvas_str += "\t\t"
+      canvas_str += "\n"
+    # Return the string
+    return canvas_str
+  
+  # Writes a pixel of the specificized color at the given coordinates
+  # TODO(Luis & Kenneth): Implement this
+  def write_pixel(self, x, y, color):
+    pass
+
+  # Returns the color of the pixel at the given coordinates
+  # TODO(Luis & Kenneth): Implement this
+  def pixel_at(self, x, y):
+    pass
+
+  # Writes a ppm file with the canvas using the specified filename
+  # TODO(Luis & Kenneth): Implement this
+  def canvas_to_ppm(self, filename):
+    pass
