23c2706d1cd6cdd7011be4ddc9146efee392f32f
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-09-20 15:30:47 -0600

    Fixed test_tuples.py

--- Ray Tracer/src/tests/test_tuples.py
+++ Ray Tracer/src/tests/test_tuples.py
@@ -6,53 +6,53 @@ from tuples import Tuple
 def test_tuples_1(): 
     t1 = Tuple()
     t2 = Tuple()
-    t1.Point(4.3, -4.2, 3.1)
-    t2.Vector(4.3, -4.2, 3.1)
-    assert True == t1.isPoint()
-    assert True == t2.isVector()
+    t1.point(4.3, -4.2, 3.1)
+    t2.vector(4.3, -4.2, 3.1)
+    assert True == t1.is_point()
+    assert True == t2.is_vector()
     
 def test_tuples_2(): 
     t1 = Tuple()
     t2 = Tuple()
-    t1.Point(4.3, -4.2, 3.1)
-    t2.Vector(4.3, -4.2, 3.1)
-    assert False == t1.isVector()
-    assert False == t2.isPoint()
+    t1.point(4.3, -4.2, 3.1)
+    t2.vector(4.3, -4.2, 3.1)
+    assert False == t1.is_vector()
+    assert False == t2.is_point()
 
 def test_tuples_add(): 
     t1 = Tuple()
     t2 = Tuple()
     expected = Tuple()
-    expected.Point(1,1,6)
-    t1.Point(3, -2, 5)
-    t2.Vector(-2, 3, 1)
+    expected.point(1,1,6)
+    t1.point(3, -2, 5)
+    t2.vector(-2, 3, 1)
     assert True == ( expected == t1 + t2 )
 
 def test_tuples_subtract(): 
     t1 = Tuple()
     t2 = Tuple()
     expected = Tuple()
-    expected.Vector(-2, -4, -6)
-    t1.Point(3, 2, 1)
-    t2.Point(5, 6, 7)
+    expected.vector(-2, -4, -6)
+    t1.point(3, 2, 1)
+    t2.point(5, 6, 7)
     assert True == ( expected == (t1 - t2))
 
 def test_tuples_subtract_vector_point():
     t1 = Tuple()
     t2 = Tuple()
     expected = Tuple()
-    t1.Point(3, 2, 1)
-    t2.Vector(5, 6, 7)
-    expected.Point(-2, -4, -6)
+    t1.point(3, 2, 1)
+    t2.vector(5, 6, 7)
+    expected.point(-2, -4, -6)
     assert True == ( expected == (t1 - t2))
     
 def test_tuples_subtract_vectors():
     t1 = Tuple()
     t2 = Tuple()
     expected = Tuple()
-    t1.Vector(3, 2, 1)
-    t2.Vector(5, 6, 7)
-    expected.Vector(-2, -4, -6)
+    t1.vector(3, 2, 1)
+    t2.vector(5, 6, 7)
+    expected.vector(-2, -4, -6)
     assert True == ( expected == (t1 - t2))
     
 def test_tuples_negating_tuples():
@@ -77,77 +77,77 @@ def test_tuples_dividing():
 
 def test_tuples_compute_magnitude():
     t1 = Tuple(1,0,0,0)
-    assert True == ( t1.equal(t1.magnitude(t1),1))
+    assert True == ( t1.equal(t1.magnitude(),1))
  
 def test_tuples_compute_magnitude2():
     t1 = Tuple(0,1,0,0)
-    assert True == ( t1.equal(t1.magnitude(t1),1))   
+    assert True == ( t1.equal(t1.magnitude(),1))   
     
 def test_tuples_compute_magnitude3():
     t1 = Tuple(0,0,1,0)
-    assert True == ( t1.equal(t1.magnitude(t1),1))  
+    assert True == ( t1.equal(t1.magnitude(),1))  
     
 def test_tuples_compute_magnitude4():
     t1 = Tuple(1,2,3,0)
-    assert True == ( t1.equal(t1.magnitude(t1),sqrt(14)))   
+    assert True == ( t1.equal(t1.magnitude(),sqrt(14)))   
     
 def test_tuples_compute_magnitude5():
     t1 = Tuple(-1,-2,-3,0)
-    assert True == ( t1.equal(t1.magnitude(t1),sqrt(14)))   
+    assert True == ( t1.equal(t1.magnitude(),sqrt(14)))   
     
 def test_tuples_normalizing_vector():
     t1 = Tuple()
-    t1.Vector(4,0,0)
+    t1.vector(4,0,0)
     expected = Tuple()
-    expected.Vector(1,0,0)
-    assert True == (expected == t1.normalize(t1))
+    expected.vector(1,0,0)
+    assert True == (expected == t1.normalize())
     
 def test_tuples_normalizing_vector2():
     t1 = Tuple()
-    t1.Vector(1,2,3)
+    t1.vector(1,2,3)
     expected = Tuple()
-    expected.Vector(1/sqrt(14),2/sqrt(14),3/sqrt(14))
-    assert True == (expected == t1.normalize(t1))  
+    expected.vector(1/sqrt(14),2/sqrt(14),3/sqrt(14))
+    assert True == (expected == t1.normalize())  
     
 def test_tuples_normalizing_vector3():
     t1 = Tuple()
-    t1.Vector(1,2,3)
-    normalized = t1.normalize(t1)
-    assert True == (normalized.equal(1, normalized.magnitude(normalized)))  
+    t1.vector(1,2,3)
+    normalized = t1.normalize()
+    assert True == (normalized.equal(1, normalized.magnitude()))  
     
 def test_tuples_dot_product_vectors():
     t1 = Tuple()
     t2 = Tuple()
-    t1.Vector(1,2,3)
-    t2.Vector(2,3,4)
-    assert True == (t1.dot(t1,t2) == 20)
+    t1.vector(1,2,3)
+    t2.vector(2,3,4)
+    assert True == (t1.dot(t2) == 20)
     
 def test_tuples_cross_product_vectors():
     t1 = Tuple()
     t2 = Tuple()
-    t1.Vector(1,2,3)
-    t2.Vector(2,3,4)
+    t1.vector(1,2,3)
+    t2.vector(2,3,4)
     expected1 = Tuple()
-    expected1.Vector(-1,2,-1)
+    expected1.vector(-1,2,-1)
     expected2 = Tuple()
-    expected2.Vector(1,-2,1)
-    assert True == (t1.cross(t1,t2) == expected1)
-    assert True == (t1.cross(t2,t1) == expected2)
+    expected2.vector(1,-2,1)
+    assert True == (t1.cross(t2) == expected1)
+    assert True == (t2.cross(t1) == expected2)
     
 def test_tuples_reflecting_vector_45():
     t1 = Tuple()
     t2 = Tuple()
     expected = Tuple()
-    t1.Vector(1, -1, 0)
-    t2.Vector(0, 1, 0)
-    expected.Vector(1, 1, 0)
-    assert True == (t1.reflect(t1, t2) == expected) 
+    t1.vector(1, -1, 0)
+    t2.vector(0, 1, 0)
+    expected.vector(1, 1, 0)
+    assert True == (t1.reflect(t2) == expected) 
 
 def test_tuples_reflecting_vector_slanted_surface():
     t1 = Tuple()
     t2 = Tuple()
     expected = Tuple()
-    t1.Vector(0, -1, 0)
-    t2.Vector(sqrt(2)/2, sqrt(2)/2, 0)
-    expected.Vector(1, 0, 0)
-    assert True == (t1.reflect(t1, t2) == expected) 
+    t1.vector(0, -1, 0)
+    t2.vector(sqrt(2)/2, sqrt(2)/2, 0)
+    expected.vector(1, 0, 0)
+    assert True == (t1.reflect(t2) == expected) 
