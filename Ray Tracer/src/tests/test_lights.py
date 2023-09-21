1ce36c57e04b9a1620ea24acfa1974111ec5c206
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-09-21 00:40:32 -0600

    Added fixed but incomplete lights and rays tests

--- Ray Tracer/src/tests/test_lights.py
+++ Ray Tracer/src/tests/test_lights.py
@@ -1,12 +1,13 @@
 import pytest
-from rayTracer.lights import PointLight
-from rayTracer.tuples import Tuple, Color
+from lights import PointLight
+from tuples import Tuple
+from colors import Color
 
 def test_point_light_with_intensity_position():
-    intensity = Color(1, 1, 1)
-    position = Tuple.point(0, 0, 0)
-    light = PointLight(position, intensity)
-    assert light.position == position
-    assert light.intensity == intensity
+  intensity = Color(1, 1, 1)
+  position = Tuple.point(0, 0, 0)
+  light = PointLight(position, intensity)
+  assert light.position == position
+  assert light.intensity == intensity
 
 
