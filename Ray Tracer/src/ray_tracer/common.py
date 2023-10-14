2b1aed60998001567c983826a4a6186b2b31666e
Luis <luisdi2509@gmail.com>
2023-10-13 20:55:22 -0600

    Created common file to hold EPSILON constant and equal function

---
+++ Ray Tracer/src/ray_tracer/common.py
@@ -0,0 +1,6 @@
+# Define a constant for the EPSILON value
+EPSILON = 0.00001
+
+# Compares two numbers and determines whether they are equal with a margin of error
+def equal(first_value, second_value):
+  return abs(first_value - second_value) < EPSILON
