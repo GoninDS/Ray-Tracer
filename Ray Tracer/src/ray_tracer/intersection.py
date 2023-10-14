70c7b238a4be92aa88f1f9430003b47ee489d5ec
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-10-13 22:19:54 -0600

    Started intersection implementation, added methods to ray

--- Ray Tracer/src/ray_tracer/intersection.py
+++ Ray Tracer/src/ray_tracer/intersection.py
@@ -1,1 +1,39 @@
 # Copyright 2023 Luis David Solano Santamaría, Kenneth Daniel Villalobos Solís
+
+from rays import Ray
+from transformations import Transformation
+from tuples import Tuple
+
+class Intersection():
+  # Default constructor
+  def __init__(self, t, object):
+    self.t = t
+    self.object = object
+
+  # Debugging representation
+  def __repr__(self):
+    return 'Intersection({}, {})'.format(self.t, self.object)
+  
+  # String representation
+  def __str__(self): 
+    return '({}, {})'.format(self.t, self.object)
+  
+  # Method to return the value of the t
+  # Used for sorting
+  def intersection_value_t(intersection):
+    return intersection.t
+
+  # Returns the hit from a list of intersections
+  @staticmethod
+  def intersections(*intersections):
+    result = list(intersections)
+    result.sort(key=intersection_value_t)
+
+  # Returns the hit from a list of intersections
+  @staticmethod
+  def hit(intersections):
+    min = None
+    for intersection in intersections:
+      if (min is None or abs(intersection.t) < min.t):
+        min = intersection
+    return min
--- Ray Tracer/src/ray_tracer/rays.py
+++ Ray Tracer/src/ray_tracer/rays.py
@@ -1,6 +1,9 @@
 # Copyright 2023 Luis David Solano Santamaría, Kenneth Daniel Villalobos Solís
 
+import math
+
 from tuples import Tuple
+from intersection import Intersection
 from transformations import Transformation
 
 class Ray:
@@ -31,3 +34,31 @@ class Ray:
     new_ray.origin = transformation * self.origin
     new_ray.direction = transformation * self.direction
     return new_ray
+  
+  # Returns the intersection bew
+  def intersect(self, object):
+    # Create a list of intersections
+    results =  []
+
+    # Calculate a, b and c
+    sphere_to_ray = self.origin - Tuple.point(0, 0, 0)
+    a = self.direction.dot(self.direction.dot)
+    b = 2 * self.direction.dot(sphere_to_ray)
+    c = sphere_to_ray.dot(sphere_to_ray)
+
+    # Calculate the discriminant
+    discriminant = (b ** 2) - (4 * a * c)
+    # If there are positive intersections
+    if discriminant >= 0:
+      # Add the first intersection
+      t1 = (-b - math.sqrt(discriminant)) / (2 * a)
+      results.append(Intersection(t1, object))
+
+      # If there were 2 intersections
+      if discriminant >= 1:
+        # Add the second intersection
+        t2 = (-b + math.sqrt(discriminant)) / (2 * a)
+        results.append(Intersection(t2, object))
+
+    # Return the list of intersections
+    return results
