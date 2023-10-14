d3b93321e210295eb7bb0906b0010d2e841bd022
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-10-14 09:36:55 -0600

    Added spheres

--- Ray Tracer/src/ray_tracer/materials.py
+++ Ray Tracer/src/ray_tracer/materials.py
@@ -1,1 +1,14 @@
 # Copyright 2023 Luis David Solano Santamaría, Kenneth Daniel Villalobos Solís
+
+class Material():
+  # Default constructor
+  def __init__(self):
+    pass
+
+  # Debugging representation
+  def __repr__(self):
+    pass
+  
+  # String representation
+  def __str__(self): 
+    pass
