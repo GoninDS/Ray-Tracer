4d2712178906b803ccd82f9ffa1daf6dc854a09a
kdaniel1652 <kennethvillalobos16@gmail.com>
2023-10-30 06:19:41 -0600

    Generated the space results and gif

--- Ray Tracer/src/main_tracer.py
+++ Ray Tracer/src/main_tracer.py
@@ -10,112 +10,35 @@ from ray_tracer.transformations import Transformation
 from ray_tracer.cameras import Camera
 import math
 
-def main():  
+def space():  
   world = World()
     
-  world.light = Light.point_light(Tuple.point(-10, 10, -10),
+  world.light = Light.point_light(Tuple.point(-100, 100, -100),
                             Color(1, 1, 1))
     
-  floor = Sphere()
-  floor.transform = Transformation.scaling(10, 0.01, 10)
-  floor.material = Material()
-  floor.material.color = Color(0.2, 0.8, 0.2)
-  floor.material.specular = 0
+  earth = Sphere()
+  earth.transform = Transformation.translation(-0.5, -1, 0.5) * Transformation.scaling(2, 2, 2)
+  earth.material = Material(Color(0.19, 0.40, 0.91), 0, 1, 1, 300)
+
+  mars = Sphere()
+  mars.transform = Transformation.translation(-3.5, 1, 10) * Transformation.scaling(1.4, 1.4, 1.4)
+  mars.material = Material(Color(0.82, 0.41, 0.12), 0, 1, 0, 300)
   
-  left_wall = Sphere()
-  left_wall.transform = Transformation.translation(0, 0, 5) * Transformation.rotation_y(-math.pi/4) * Transformation.rotation_x(math.pi/2) * Transformation.scaling(10, 0.01, 10)
-  left_wall.material = Material(Color(0.68, 0.85, 0.9), 1, 0, 0, 200)
-    
-  right_wall = Sphere()
-  right_wall.transform = Transformation.translation(0, 0, 5) * Transformation.rotation_y(math.pi/4) * Transformation.rotation_x(math.pi/2) * Transformation.scaling(10, 0.01, 10)
-  right_wall.material = left_wall.material
-    
-  body = Sphere()
-  body.transform = Transformation.translation(-0.5, 0.5, 0.5) * Transformation.scaling(0.5, 0.5, 0.5)
-  body.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
+  moon = Sphere()
+  moon.transform = Transformation.translation(1.5, 1, 1.5) * Transformation.scaling(0.7, 0.7, 0.7)
+  moon.material = Material(Color(0.33, 0.33, 0.33), 0, 1, 0, 0)
+  
+  world.objects.append(earth)
+  world.objects.append(mars)
+  world.objects.append(moon)
 
-  head = Sphere()
-  head.transform = Transformation.translation(-0.5, 1.2, 0.5) * Transformation.scaling(0.3, 0.3, 0.3)
-  head.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  right_paw = Sphere()
-  right_paw.transform = Transformation.translation(-0.2, 0.1, 0.5) * Transformation.scaling(0.2, 0.1, 0.4)
-  right_paw.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  left_paw = Sphere()
-  left_paw.transform = Transformation.translation(-0.8, 0.1, 0.5) * Transformation.scaling(0.2, 0.1, 0.4)
-  left_paw.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  right_arm = Sphere()
-  right_arm.transform = Transformation.translation(-0.3, 0.75, 0.3) * Transformation.scaling(0.2, 0.1, 0.4)
-  right_arm.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  left_arm = Sphere()
-  left_arm.transform = Transformation.translation(-0.7, 0.75, 0.3) * Transformation.scaling(0.2, 0.1, 0.4)
-  left_arm.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  right_ear = Sphere()
-  right_ear.transform = Transformation.translation(0, 1.4, 0.5) * Transformation.rotation_z((math.pi *3) / 2) * Transformation.scaling(0.1, 0.4, 0.1)
-  right_ear.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  left_ear = Sphere()
-  left_ear.transform = Transformation.translation(-0.8, 1.6, 0.5) * Transformation.rotation_z(math.pi / 4) * Transformation.scaling(0.1, 0.4, 0.1)
-  left_ear.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  tail_base = Sphere()
-  tail_base.transform = Transformation.translation(0, 0.5, 0.8) * Transformation.scaling(0.4, 0.1, 0.01)
-  tail_base.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  tail_body = Sphere()
-  tail_body.transform = Transformation.translation(0.5, 0.9, 0.8) * Transformation.scaling(0.12, 0.4, 0.01) * Transformation.shearing(1, 0, 0, 0, 0, 0)
-  tail_body.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  tail_tip = Sphere()
-  tail_tip.transform = Transformation.translation(0.86, 1.3, 0.8) * Transformation.scaling(0.4, 0.2, 0.01)
-  tail_tip.material = Material(Color(1, 0.902, 0.176), 0.3, 0.6, 0, 50)
-
-  left_eye = Sphere()
-  left_eye.transform = Transformation.translation(-0.39, 1.4, -1.1) * Transformation.scaling(0.025, 0.025, 0.01)
-  left_eye.material = Material(Color.black(), 0, 0, 1, 200)
-
-  right_eye = Sphere()
-  right_eye.transform = Transformation.translation(-0.3, 1.4, -1.1) * Transformation.scaling(0.025, 0.025, 0.001)
-  right_eye.material = Material(Color.black(), 0, 0, 1, 200)
-
-  left_cheek = Sphere()
-  left_cheek.transform = Transformation.translation(-0.45, 1.3, -1.0) * Transformation.scaling(0.025, 0.025, 0.01)
-  left_cheek.material = Material(Color(0.91, 0.16, 0.16), 0.3, 0.6, 0, 50)
-
-  right_cheek = Sphere()
-  right_cheek.transform = Transformation.translation(-0.25, 1.3, -1.0) * Transformation.scaling(0.025, 0.025, 0.001)
-  right_cheek.material = Material(Color(0.91, 0.16, 0.16), 0.3, 0.6, 0, 50)
-    
-  world.objects.append(floor)
-  world.objects.append(left_wall) 
-  world.objects.append(right_wall)
-  world.objects.append(body)
-  world.objects.append(head)
-  world.objects.append(right_paw)
-  world.objects.append(left_paw)
-  world.objects.append(right_arm)
-  world.objects.append(left_arm)
-  world.objects.append(right_ear)
-  world.objects.append(left_ear)
-  world.objects.append(tail_base)
-  world.objects.append(tail_body)
-  world.objects.append(tail_tip)
-  world.objects.append(left_eye)
-  world.objects.append(right_eye)
-  world.objects.append(left_cheek)
-  world.objects.append(right_cheek)
-
-  camera = Camera(300, 150, math.pi/3)
+  camera = Camera(900, 450, math.pi/3)
   camera.transformation_matrix = Transformation.view_transform(Tuple.point(0, 1.5, -5),
     Tuple.point(0, 1, 0),
     Tuple.vector(0, 1, 0))
 
   canvas = camera.render_parallel(world)
-  canvas.canvas_to_ppm("pikachu43.ppm")  
+  canvas.canvas_to_ppm("space_big22.ppm")  
 
 if __name__ == "__main__":
-  main()
+  space()
